71f0a5f5f634300714a22f000e1ad4211a5fb235
Marcos Camargo <marcoscamargo@conectala.com.br>
2022-12-01 23:19:54 -0300

    Finalização do layout da sesssão Welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,13 +4,28 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from '.';
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step='0.0001'
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
 const Welcome = () => {
   const connectWallet = () => {
 
   }
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div className='flex w-full justify-center items-center'>
-      <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
@@ -50,8 +65,7 @@ const Welcome = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className='flex justify-between items-start'>
@@ -71,10 +85,28 @@ const Welcome = () => {
             </div>
           </div>
 
-          <div className='p-5'>
+          <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+            <Input placeholder='Address to' name='addresTo' type='text' handleChenge={() => { }} />
+            <Input placeholder='Amount (ETH)' name='amount' type='number' handleChenge={() => { }} />
+            <Input placeholder='Keyword (Gif)' name='keyword' type='text' handleChenge={() => { }} />
+            <Input placeholder='Enter Message' name='message' type='text' handleChenge={() => { }} />
 
+            <div className='h-[1px] w-full bg-gray-400 my-2' />
+
+            {true ? (
+              <Loader />
+            ) : (
+              <button
+                type='button'
+                onClick={handleSubmit}
+                className='text-white w-full mt-2 border-[1px] p-2 border[#3d4f7c] rounded-full cursor-pointer'
+              >
+                Send Now
+              </button>
+            )}
           </div>
         </div>
+      </div>
     </div>
   );
 }
