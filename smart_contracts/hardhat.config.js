678a45f716db122eccbcd97ff354690e17664162
Marcos Camargo <marcoscamargo@conectala.com.br>
2022-12-03 00:06:32 -0300

    Deploy do smart contract com Goerli API

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
 
+// require('.env').config();
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+     hardhat: {},
+     goerli: {
+        url:process.env.API_URL,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+     }
+  },
 };
